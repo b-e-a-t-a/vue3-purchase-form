bee0ea6e138858649118e2e8821b8ec6f5b0ed73
Beata Karolczak <karolczak.beata@gmail.com>
2023-10-06 14:06:58 +0200

    Create project

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <HomeView />
+  </div>
+</template>
+
+<script setup>
+import HomeView from "./views/HomeView.vue"
+</script>
+
+
+<style lang="sass">
+$action-color: #5cc41e
+$background-color: #dcdcdc
+$error-color: #cc0000
+$placeholder-color: #949494
+
+
+.container
+  background-color: $background-color
+  width: 100%
+  padding: 10px 40px
+  max-width: 850px
+  margin: 0 auto
+  @media (min-width: 900px)
+    padding-top: 72px
+.wrapper
+  min-height: 100vh
+  padding: 0 20px
+  display: flex
+  flex-direction: column
+  flex: 1
+  position: relative
+  @media (min-width: 900px)
+    flex-direction: row !important
+
+h2
+  text-transform: uppercase
+
+@media (min-width: 1024px)
+  .wrapper
+    display: flex
+    place-items: flex-start
+    flex-wrap: wrap
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <main>
+    <h2>Personal Information</h2>
+    <div class="wrapper">
+      <section class="form">
+        FORM SECTION
+        <transition name="form">
+          <form>
+            <label>First name</label>
+            <input
+              placeholder="Elon"
+            />
+            <button>
+              <i class="fa-solid fa-cart-shopping" style="color: white;"></i>
+              <span>Next</span>
+            </button>
+          </form>
+        </transition>
+      </section>
+      <section>
+        ORDER SECTION
+      </section>
+    </div>
+  </main>
+</template>
+
+<script setup>
+
+</script>
+
